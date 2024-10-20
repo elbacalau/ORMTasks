b4b18a71064f6d05e3a775dbdb2bf49941d846ff
Elbacalau <oriolreverte36@gmail.com>
2024-10-20 17:39:12 +0200

    updated register comopnent to make it functional and added new password validator TODO: Add confirm password and user name field

--- ormtasks.client/src/app/pages/register_screen/register-component/register-component.component.ts
+++ ormtasks.client/src/app/pages/register_screen/register-component/register-component.component.ts
@@ -1,19 +1,19 @@
 import { AuthService } from '../../../services/user_services/auth.service';
-import { JsonPipe } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { JsonPipe, NgClass } from '@angular/common';
+import { Component, OnInit, signal } from '@angular/core';
 import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { Router, RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "../../components/navbar/navbar/navbar.component";
 
 @Component({
   selector: 'app-register-component',
   standalone: true,
-  imports: [RouterOutlet, ReactiveFormsModule, JsonPipe],
-  templateUrl: './register-component.component.html',
+  imports: [RouterOutlet, ReactiveFormsModule, JsonPipe, NavbarComponent, NgClass],
+  templateUrl: './register.component.html',
 
 })
-export class RegisterComponent {
-
-
+export class RegisterComponent implements OnInit{
+expression: any;
 
   constructor(
     private authService: AuthService,
@@ -21,15 +21,20 @@ export class RegisterComponent {
   ) { }
 
 
+  ngOnInit(): void { }
+
+
   errorMessage = signal("")
   isLoading = signal(false)
+  showPasswordError = signal(false);
+
   registerForm = signal<FormGroup>(
     new FormGroup({
       nombre: new FormControl('', Validators.required),
       apellido: new FormControl('', Validators.required),
       segundoApellido: new FormControl(''),
       email: new FormControl('', Validators.required),
-      contrasena: new FormControl('', Validators.required),
+      contrasena: new FormControl('', [Validators.required, this.passwordValidator]),
       fechaNacimiento: new FormControl('', Validators.required),
       ciudad: new FormControl('', Validators.required),
       poblacion: new FormControl('', Validators.required),
@@ -37,15 +42,37 @@ export class RegisterComponent {
     })
   );
 
+  passwordValidator(control: FormControl): { [key: string]: boolean } | null {
+    const password: string = control.value;
+    const hasMinLength = password && password.length >= 8;
+    const hasNumber = /\d/.test(password);
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+
+    const valid = hasMinLength && hasNumber && hasUpperCase && hasSpecialChar;
+
+    return valid ? null : { invalidPassword: true };
+  }
+
+  onPasswordChange(): void {
+    const passwordControl = this.registerForm().get('contrasena');
+    if (passwordControl) {
+      this.showPasswordError.set(passwordControl.errors?.['invalidPassword'] === true);
+    }
+  }
+
+
 
   onSubmit(): void {
+    this.showPasswordError.set(false);
+
     if (this.registerForm().valid) {
       this.isLoading.set(true);
 
       this.authService.createUser(this.registerForm())
         .subscribe({
           next: response => {
-            console.log('Usuario creado:', response);
+            console.log('Usuario creado con exito');
             this.router.navigate(['/login']);
           },
           error: error => {
@@ -57,13 +84,10 @@ export class RegisterComponent {
           }
         });
     } else {
+      if (this.registerForm().get('contrasena')?.errors?.['invalidPassword']) {
+        this.showPasswordError.set(true);
+      }
       console.error("Formulario no válido:", this.registerForm().errors);
     }
   }
-
-
-
-
-
-
 }
