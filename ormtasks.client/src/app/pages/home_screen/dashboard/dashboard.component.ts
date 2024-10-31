b8b1f8c1d7eba2edfc951ecb70c69e3718accdc9
oriolreverte36 <oreverte@avanzaprl.es>
2024-10-31 01:06:11 +0100

    created new first entry component if user is the first time to use the app

--- ormtasks.client/src/app/pages/home_screen/dashboard/dashboard.component.ts
+++ ormtasks.client/src/app/pages/home_screen/dashboard/dashboard.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 import { UserService } from 'src/app/services/user_services/user.service';
-import { HttpClient } from '@angular/common/http';
 import { User } from 'src/app/interfaces/user.interface';
+import { FirstEntryComponent } from "../../components/first-entry/first-entry.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [SidebarComponent, RouterModule],
+  imports: [SidebarComponent, RouterModule, FirstEntryComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
@@ -22,7 +22,6 @@ export class DashboardComponent implements OnInit{
       this.userData = data;
     });
   }
-
 
 
 
